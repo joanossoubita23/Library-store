be6313995d50fc03cf0512483b5b52b4957539f9
joanossoubita23 <joanossoubita@yahoo.fr>
2023-12-27 20:57:06 -0500

    get-setter

--- src/main/java/Book.java
+++ src/main/java/Book.java
@@ -1,2 +1,55 @@
-package PACKAGE_NAME;public class Book {
+public class Book {
+    private int bookId;
+    private String title;
+    private String author;
+    private double price;
+
+    public int getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(int bookId) {
+        this.bookId = bookId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public Book(int bookId, String title, String author, double price) {
+        this.bookId = bookId;
+        this.title = title;
+        this.author = author;
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "bookId=" + bookId +
+                ", title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", price=" + price +
+                '}';
+    }
 }
